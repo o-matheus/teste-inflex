2b067ad2f3310b02975d25555487b8efe8c680fc
Matheus <matheusaguiar028@gmail.com>
2026-09-22 12:05:55 -0300

    feat(main): quantiadde de salários mínimos que cada funcionário recebe

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static final BigDecimal SALARIO_MINIMO = new BigDecimal("1212.00");
 
     static void main() {
         List<Funcionario> funcionarios = new ArrayList<>();
@@ -34,6 +36,10 @@ public class Main {
         System.out.print("\nDepois do aumento de salário");
         imprimirFuncionarios(funcionarios);
 
+
+        System.out.printf("%nFuncionário - Quantidade de Salários Minimos.%n");
+        quantidadeSalariosMinimos(funcionarios);
+
     }
 
     public static void imprimirFuncionarios(List<Funcionario> funcionarios) {
@@ -49,6 +55,13 @@ public class Main {
         for(Funcionario funcionario : funcionarios) {
             BigDecimal novoSalario = funcionario.getSalario().multiply(valorAumento);
             funcionario.setSalario(novoSalario);
+        }
+    }
+
+    public static void quantidadeSalariosMinimos(List<Funcionario> funcionarios) {
+        for (Funcionario funcionario : funcionarios) {
+            BigDecimal qtdSalarios = funcionario.getSalario().divide(SALARIO_MINIMO, 2, RoundingMode.HALF_UP);
+            System.out.printf("%s ganha %s salários minimos.%n", funcionario.getNome(), qtdSalarios);
         }
     }
 }
